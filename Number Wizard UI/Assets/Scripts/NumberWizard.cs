80572b5a58c79bad35ef0d4c31a7f627895e9a67
LordSayur <mohammad.omar.317@gmail.com>
2019-02-17 15:17:28 +0800

    Import & refactor code

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        guess = (max + min) / 2;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    void StartGame()
+    {
+        guess = 500;
+        max = max + 1;
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+}
